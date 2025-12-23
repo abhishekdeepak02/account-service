eef55842888529cd57967a935f65cfb1c9fe6dcf
abhishekdeepak02 <yadu.akd@gmail.com>
2025-12-23 22:38:09 +0530

    implemented API documentation, jpa auditing and input-output validation

--- src/main/java/com/lazycoder/account/controller/AccountsController.java
+++ src/main/java/com/lazycoder/account/controller/AccountsController.java
@@ -2,23 +2,55 @@ package com.lazycoder.account.controller;
 
 import com.lazycoder.account.constants.AccountsConstants;
 import com.lazycoder.account.dto.CustomerDto;
+import com.lazycoder.account.dto.ErrorResponseDto;
 import com.lazycoder.account.dto.ResponseDto;
 import com.lazycoder.account.service.IAccountsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "Accounts REST APIs for CRUD operations",
+        description = "CRUD REST APIs for Lazy bank to CREATE, UPDATE, FETCH and DELETE account information"
+)
 @RestController
 @RequestMapping(path = "/api/account", produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
+@Validated
 public class AccountsController {
 
     private IAccountsService iAccountsService;
 
+    @Operation(
+            summary = "Create Account REST API",
+            description = "REST API to create account for a customer in Lazy Bank"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status INTERNAL SERVER ERROR",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "HTTP Status 201 CREATED"
+            )}
+    )
     @PostMapping("/create")
-    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+    public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
 
         iAccountsService.createAccount(customerDto);
         return ResponseEntity
@@ -26,8 +58,26 @@ public class AccountsController {
                 .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 
+    @Operation(
+            summary = "Fetch Account REST API",
+            description = "REST API to fetch account info for a account holder in Lazy Bank."
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status INTERNAL SERVER ERROR",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    ))
+    })
     @GetMapping("/fetch")
-    public ResponseEntity<CustomerDto> fetchAccount(@RequestParam String mobileNumber) {
+    public ResponseEntity<CustomerDto> fetchAccount(@RequestParam
+            @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile Number must be 10 digit.")
+                                                        String mobileNumber) {
 
         CustomerDto customerDto = iAccountsService.fetchAccount(mobileNumber);
         return ResponseEntity
@@ -35,28 +85,73 @@ public class AccountsController {
                 .body(customerDto);
     }
 
+    @Operation(
+            summary = "Update Account REST API",
+            description = "REST API to update account info for a existing account holder in Lazy Bank."
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "HTTP Status EXPECTATION FAILED"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status INTERNAL SERVER ERROR",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+
+            )
+    })
     @PutMapping("/update")
-    public ResponseEntity<ResponseDto> updateAccount(@RequestBody CustomerDto customerDto) {
+    public ResponseEntity<ResponseDto> updateAccount(@Valid @RequestBody CustomerDto customerDto) {
 
         boolean updateAccount = iAccountsService.updateAccount(customerDto);
         if (updateAccount) {
             return ResponseEntity.status(HttpStatus.OK)
                     .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
         } else {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseDto(AccountsConstants.STATUS_500, AccountsConstants.MESSAGE_500));
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_UPDATE));
         }
     }
 
+    @Operation(
+            summary = "DELETE Account REST API",
+            description = "REST API to delete account info for a existing account holder in Lazy Bank."
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "HTTP Status EXPECTATION FAILED"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status INTERNAL SERVER ERROR",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
     @DeleteMapping("/delete")
-    public ResponseEntity<ResponseDto> deleteAccountInfo(@RequestParam String mobileNumber) {
+    public ResponseEntity<ResponseDto> deleteAccountInfo(@RequestParam
+            @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile Number must be 10 digit.")
+                                                             String mobileNumber) {
         boolean deleteAccount = iAccountsService.deleteAccount(mobileNumber);
         if(deleteAccount) {
             return ResponseEntity.status(HttpStatus.OK)
                     .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
         }else {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseDto(AccountsConstants.STATUS_500, AccountsConstants.MESSAGE_500));
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_DELETE));
         }
 
     }
--- src/main/java/com/lazycoder/account/dto/ResponseDto.java
+++ src/main/java/com/lazycoder/account/dto/ResponseDto.java
@@ -1,13 +1,24 @@
 package com.lazycoder.account.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@Schema(
+        name = "Response",
+        description = "Schema to hold standard response information"
+)
 @Data
 @AllArgsConstructor
 public class ResponseDto {
 
+    @Schema(
+            description = "Status Code of the Response"
+    )
     private String statusCode;
 
+    @Schema(
+            description = "Status Message of the Response"
+    )
     private String statusMsg;
 }
