f5b62b93b8312d778fb35d25988bca6e54e2133d
abhishekdeepak02 <yadu.akd@gmail.com>
2026-01-04 21:58:50 +0530

    Adding eureka client configuration

--- src/main/java/com/lazybytes/account/AccountApplication.java
+++ src/main/java/com/lazybytes/account/AccountApplication.java
@@ -13,7 +13,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients // Registering service as eureka client
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @EnableConfigurationProperties(value = {AccountContactRecordInfo.class})
 @OpenAPIDefinition(
